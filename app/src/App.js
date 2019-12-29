31c7531681e54c0adb089a4ab2a6d102f7df9fd9
Mikhail Slivinskiy <m.slivinskiy@gmail.com>
2019-12-29 17:47:58 +0100

    generate test data

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import Data from './testData/testData.js'
+
 function App() {
+  console.log(Data.inputArr);
   return (
     <div>
       <div className="App">Hello World</div>
