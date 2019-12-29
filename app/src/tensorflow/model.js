5f0a8bf80a025dce8a9aa2bfffe22fe366b6fca8
David Bunk <davidolafbunk@gmail.com>
2019-12-29 18:05:10 +0100

    Fix in model.js

--- app/src/tensorflow/model.js
+++ app/src/tensorflow/model.js
@@ -1,7 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
 import Data from '../testData/testData.js'
 
-const async function = trainModel(inputs, outputs, size, window_size, n_epochs, learning_rate, n_layers, callback) => {
+const async function trainModel = (inputs, outputs, size, window_size, n_epochs, learning_rate, n_layers, callback) => {
 	const input_layer_shape = window_size;
 	const input_layer_neurons = 100;
 
