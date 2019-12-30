ba0744711f962c629f3512a2c6c29f665efd8130
Mikhail Slivinskiy <m.slivinskiy@gmail.com>
2019-12-30 02:00:17 +0100

    Add basic csv upload

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import Data from './testData/testData.js'
 import Model from './tensorflow/model.js'
 
- const functionName = () => {
 
- }
 
-function App() {
-	Model.trainModel(Data.inputArr, Data.targetArr, 15, 15, 250, 0.005, 4).then(
+const App = () => {
+  // hosting: https://www.freecodecamp.org/news/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe/
+  
+  /*
+	Model.trainModel(Data.inputArr, Data.targetArr, 15, 15, 1000, 0.005, 4).then(
 		(wrapper_train) => {
     console.log(Model.Predict(Data.inputArr, 15, wrapper_train.model))
   });
+  */
+
+  const [textFromFileLoaded, setTextFromFileLoaded] = useState('');
   
+
+
+
+  const loadFileAsText = () => {
+    const fileToLoad = document.getElementById("fileToLoad").files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (fileLoadedEvent) => {
+      const textFromFileLoaded = fileLoadedEvent.target.result;
+      console.log(textFromFileLoaded);
+      setTextFromFileLoaded(textFromFileLoaded);
+    };
+
+    fileReader.readAsText(fileToLoad, "UTF-8");
+  }
+
   return (
     <div className="App">
-      <div className="InputHeader">
-        <table>
-          <tr>
-            <th><input type="text" name="name" /></th>
-            <th>Lastname</th>
-            <th>Age</th>
-          </tr>
-        </table>
-      </div>
+      {textFromFileLoaded.length <=0 && <input type="file" accept=".csv" onChange={loadFileAsText} id="fileToLoad" />}
+      {textFromFileLoaded.length > 0 && <div>{textFromFileLoaded} </div>}
     </div>
   );
 }
